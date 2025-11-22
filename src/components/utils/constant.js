af37e353379c8996e5e7dcff5f990d75367e698b
sajal <sajalku442@gmail.com>
2025-11-22 19:05:26 +0530

    Api

--- src/components/utils/constant.js
+++ src/components/utils/constant.js
@@ -1,9 +1,13 @@
-export const USER_API_END_POINT = "http://localhost:5000/api/user";
-export const CASEBLOG_API_END_POINT = "http://localhost:5000/api/blog";
-export const INSIGHT_API_END_POINT = "http://localhost:5000/api/insight";
-export const CONTACT_API_END_POINT = "http://localhost:5000/api/contact";
+// export const USER_API_END_POINT = "http://localhost:5000/api/user";
+// export const CASEBLOG_API_END_POINT = "http://localhost:5000/api/blog";
+// export const INSIGHT_API_END_POINT = "http://localhost:5000/api/insight";
+// export const CONTACT_API_END_POINT = "http://localhost:5000/api/contact";
 
-// export const USER_API_END_POINT ="https://tecg-company-api.onrender.com/api/user";
-// export const CASEBLOG_API_END_POINT ="https://tecg-company-api.onrender.com/api/blog";
-// export const INSIGHT_API_END_POINT ="https://tecg-company-api.onrender.com/api/insight";
-// export const CONTACT_API_END_POINT ="https://tecg-company-api.onrender.com/api/contact";
+export const USER_API_END_POINT =
+  "https://tecg-company-api.onrender.com/api/user";
+export const CASEBLOG_API_END_POINT =
+  "https://tecg-company-api.onrender.com/api/blog";
+export const INSIGHT_API_END_POINT =
+  "https://tecg-company-api.onrender.com/api/insight";
+export const CONTACT_API_END_POINT =
+  "https://tecg-company-api.onrender.com/api/contact";
